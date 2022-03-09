a557725b8a795a75bf9101b37dd80692f3299b2e
wangqun28 <wangqun6@jd.com>
2022-03-09 14:50:25 +0800

    change

--- src/static/js/brand/home/main.jsx
+++ src/static/js/brand/home/main.jsx
@@ -1,9 +1,25 @@
-const p = new Promise(res => {
-    setTimeout(() => {
-        res('成功');
-    }, 3000);
+import { Chart } from '@antv/g2';
+
+const data = [
+    { genre: 'Sports', sold: 275 },
+    { genre: 'Strategy', sold: 115 },
+    { genre: 'Action', sold: 120 },
+    { genre: 'Shooter', sold: 350 },
+    { genre: 'Other', sold: 150 }
+];
+
+// Step 1: 创建 Chart 对象
+const chart = new Chart({
+    container: 'main', // 指定图表容器 ID
+    width: 600, // 指定图表宽度
+    height: 300 // 指定图表高度
 });
 
-p.then(value => {
-    console.log(value);
-});
+// Step 2: 载入数据源
+chart.data(data);
+
+// Step 3: 创建图形语法，绘制柱状图
+chart.interval().position('genre*sold');
+
+// Step 4: 渲染图表
+chart.render();
